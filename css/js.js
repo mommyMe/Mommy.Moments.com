11f423f9bd709721cf63962f501bb2e669f719fe
mommyMe <89680125+mommyMe@users.noreply.github.com>
2021-08-30 04:11:05 +0600

    Add files via upload

---
+++ css/js.js
@@ -0,0 +1,913 @@
+// console.log('hello');
+// const name='arfan';
+// const greeting='Good night';
+// console.log(greeting+' ' + name);
+
+
+
+
+
+
+
+// let html;
+// html="<h1>This is header</h1>"+
+// "<p>This id pera</p>";
+// html=html.concat('this','stri');
+// console.log(html);
+// // console.log(html.indexOf('h1'));
+// // console.log(html.lastIndexOf('>'));
+
+// let she='mommy';
+// let her='nusrat';
+// let title=`
+// <h1>She is ${her} ${she}</h1>
+// `;
+// document.body.innerHTML=title;
+
+
+// let marks = [34,4,322,4,98];
+// const fruits = ['orange','tomato','apple'];
+// const arr = new Array(34,443,44,23,'orange')
+// const mixed =['stri' ,[43,34,34],34];
+// console.log(mixed);
+// console.log(fruits[1]);
+// console.log(Array.isArray(arr));
+// arr[0]='arfan'
+// console.log(arr);
+
+
+// let value =fruits.indexOf('tomato')
+// console.log(value)
+
+// fruits.push('yello');
+// console.log(fruits);
+
+
+// marks.unshift(49302);
+// // console.log(marks)
+
+// // marks.pop();
+// // console.log(marks);
+
+// // marks.splice(1,2);
+// // console.log(marks);
+
+// // marks.reverse();
+// // console.log(marks);
+
+// let marks2=[3,4,3,3,3]
+// marks=marks.concat(marks2)
+
+// console.log(marks);
+
+
+// let myobj={
+//         name:'Arfan',
+//         she:'mommy'
+   
+// }
+// console.log(myobj);
+// console.log(myobj['name'])
+// console.log(myobj.she);
+
+// if else 
+// const agee=3;
+// const vari="canDrive";
+// const doesDrive=false;
+// if (agee==18){
+//         console.log('You are adult')
+// }
+// else if (agee<=18){
+//         console.log('you are not adult')
+// }
+// else {
+//         console.log('you are adult')
+// }
+// if (typeof vari !=='undefinedf'){
+//         console.log('vari is  undefined')
+// }
+// else{
+//         console.log('vari is not undefined')
+// }
+
+
+
+
+// if (doesDrive || age > 18){
+//         console.log('can Drive')
+// }
+
+// else if(doesDrive && age<18 ){
+//         console.log('you cannot drive')
+// }
+
+// console.log(age>18? 'age is ':'age is not ');
+
+
+// var age=-10;
+// switch (true) {
+//         case (age>=18 && age>=100 ):
+//                 console.log('you are adult');
+//                 break;
+
+//                 case (age<=0 && age<=18):
+//                 console.log('you are not adult')
+//                         break;
+//                         case (age<=0 && age<=18):
+//                                 console.log('you are not adult')
+//                                         break;
+        
+//         default:
+//                 console.log('Enter the valid age')
+//                 break;
+// }
+
+// for(let i=0; i<18;i++){
+//      console.log(i)
+// }
+
+
+// for(var a=0;a<=100;a++){
+//     document.write("sorry"+"<br>")
+// }
+// let m=1
+// do{
+//         console.log(m+1);
+//         if(m===4){
+//                 break;
+//         }
+//         m+=1
+// }while(m<10)
+
+
+
+// let u=1;
+// while(u<8){
+//         console.log(u);
+//         u+=1
+// }
+
+
+
+
+// function 
+// const mygreet=function(name,thank='thank you'){
+//   let msg =`hello my is ${name} ${thank}`;
+//         return msg;
+// }
+
+// let name='Arfan';
+// let val=mygreet(name, 'thanks a lot');
+// console.log(val);
+  
+// let nasme={
+//         mynasme:"arfan",
+//         game:function(){
+//          return "hehe";
+//         }
+// }
+// console.log(nasme.game());
+
+
+// function greety(namee){
+//         console.log(`helloooooo${namee}ooooooooooooooo`);
+// }
+// let namee='heheheheheheh'
+// greety(namee);
+
+
+
+// arr =['arfan','mommy','maidah'];
+// arr.forEach(function (element,index,array){
+//         console.log(element,index);
+// });        
+        
+
+
+// let age=12;
+// switch (age) {
+//         case (age>18):
+//                 console.log("You are adult")
+//                 break;
+
+//                 case (age<18):
+//                         console.log("You are not adult")
+//                         break;
+
+//         default:
+//                console.log("enter Your age")
+//                 break;
+// }
+// let u=44;
+// switch (u) {
+//     case u>=22:
+//         document.write("you are a")
+        
+//         break;
+//         case u>92:
+//             document.write("you are b")
+            
+//             break;    case u>220:
+//             document.write("you are c")
+            
+//             break;
+//     default:  document.write("you are 3ec")
+//         break;
+// }
+// alart 
+
+// var h = confirm("hello?");
+
+// if(h){
+//     alert("THANKS")
+// }
+
+
+
+
+
+// else{
+//     alert('sorry')
+// }
+
+
+// var p=prompt("Enter your marks");
+
+
+// if (p<=30){
+//     document.write("You are fail")
+// }
+// else if(p>=30  && p<=50) {
+//     document.write("You are hopefully passed")
+
+// }
+// else if(p>=50  && p<=80) {
+//     document.write("A-")
+
+// }
+// else if(p>=80  && p<=100) {
+//     document.write("A+")
+
+// }
+// else{
+//     document.write("Enter valid marks")
+// }
+
+
+// var age=prompt("Enter your age");
+// if (age>=18){
+//     document.getElementsByTagName("body").style.color="red";
+// }
+// else{
+//     document.getElementsByTagName("body").style.color="green";
+
+// }
+
+
+
+
+
+// function ------------------------------------------------
+
+// function  fnamee() {
+//     document.write("Hello world")
+// }
+
+
+// fnamee()
+// document.write("<br>")
+// fnamee()
+
+// function ends 
+
+
+// fuunction with perameters  ---------------------------------------------
+
+// document.write("<br>")
+// function nam(a,b) {
+//   document.write(a+b)
+    
+// }
+// nam(3,5)
+
+
+
+// return value --------------------------------
+
+
+// function nums(bio,ict,chem) {
+//     var ttl=bio+ict+chem;
+//     return ttl;
+// }
+// function persent(ttl) {
+//     var b=ttl/3;
+//     document.write(b);
+// }
+
+// var plus=nums(12,12,12)
+// persent(plus)
+
+// ============
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.write("<br>")
+// function inOne(eng,ban,sum) {
+//     document.write(((eng+ban+sum)/300)*100);
+// }
+// inOne(33,22,22)
+
+// ==========
+
+
+
+
+// local variables and global variables-----------------------
+
+// while loop ---------------------------------
+
+
+// var a=0;
+// document.write("<ul>")
+// while(a<=1090){
+//     document.write(a)
+//     a++
+// }
+// document.write("</ul>")
+
+
+
+
+// do while loop -----------------------------------
+
+// var a=10;
+// do{
+//     document.write("Hello<br>");
+//     a++
+// }
+// while(a<=100);
+
+
+
+// for loop ------------------------------------
+
+// for(var a=10;a<=100;a++){
+//     document.write("hello <br>")
+// }
+
+
+
+// break and continue steatment---------------------------------
+
+// for(var a=1;a<=100;a++){
+//     if(a==3){
+//        console.log("hi")
+//         continue
+//     }
+//     console.log(a)
+// }
+
+
+// even and odd --------------------------------------------
+
+// for(a=1;a<=100;a++){
+//     if(a%2==0){
+//         document.write(a+"<br>")
+//     }
+// }
+
+// for(var a=1; a<=100;a++){
+//      if(a % 2 != 0){
+//          document.write(a+"<br>")
+//      }
+// }
+
+
+
+// nasted loop -----------------------------------------------
+// for(a=1;a<=100;a=a+10){
+//   for(b=1;b<=a;b++){
+//       document.write(b)
+//   }
+//   document.write("<br>")
+// }
+
+// for(a=0;a<10;a++){
+//     for(b=1;b<=a;b++){
+//         document.write(b)
+//     }
+//     document.write("<br>");
+// }
+
+// for(a=0;a<=10;a++){
+//     for( b=1;b<=a;b++){
+//         document.write(a)
+//     }
+//     document.write("<br>")
+
+// }
+
+// for(a=1;a<=5;a++){
+//      for(b=1;b<=a;b++){
+//          document.write(a)
+//      }
+// }
+
+// for(var a=5;a>=1;a--){
+//     for(var b=1;b<=a;b++){
+//         document.write(a);
+//     }
+//     document.write("<br>")
+// }
+
+// for(var a=1;a<=100;a++){
+//     for(var b=1;b<=a;b++){
+//     document.write(b)
+//     }
+//     document.write("<br>")
+// }
+
+
+
+
+// for(var a=5;a>=1;a--){
+//     for(var b=1; b<=a;b++){
+//         document.write(a)
+//     }
+//     document.write("<br>")
+// }
+
+
+// arrays -----------------------------
+
+// var array=[30,39,32,29,45,24];
+// document.write("<ul>")
+// var sum=0;
+// for(a=0;a<=5;a++){
+//     document.write("<li>"+array[a]+"</li>");
+//     sum=sum+array[a];
+// }
+// document.write("</ul>")                                       
+// document.write("total marks is:"+sum);
+
+
+// var arry=new Array(3);
+// for(var g=0;g<3;g++)
+// {
+//     arry[g]=prompt("Enter first password");
+// }
+
+// document.write("<ul>")
+// for(var a=0;a<3;a++){
+//      document.write("<li>"+arry[a]+"</li>")
+// }
+// document.write("</ul>")
+
+
+// multidimensional arrays -----------------------------
+
+// var array=[
+//     ["arfan",19,2021,"Dhaka"],
+//     ["arfan",19,2021,"Dhaka"],
+//     ["arfan",19,2021,"Dhaka"],
+//     ["arfan",19,2021,"Dhaka"]
+
+// ]
+// document.write("<table border='1px' cellspacing='1'>")
+
+// for( var a=0;a<array.length;a++){
+// document.write("<tr>")
+
+//     for(var b=0;b<array[a].length;b++)
+//     document.write("<td>"+array[a][b]+"</td>")
+// document.write("</tr>")
+
+
+// }
+
+// document.write("</table>")
+
+
+// changing array values---------------------
+
+// var arry=["heello","bye","hi"]
+// document.write(arry+"</br>");
+
+// arry[0]="hello";
+
+// document.write(arry+"</br>");
+
+// delete  arry[1]
+// document.write(arry+"</br>");
+
+
+
+
+// concat and join _----------------------------
+
+
+// var a=["hello","abbe","tere"];
+
+// var b=["oy","oaw"];
+// var c=a.concat(b,"hee")
+// document.write(c+"<br>")
+
+// var d=c.join("/");
+// document.write(d)
+
+
+
+
+// slice splice -------------------------------------------
+// var a=["safi","Rahat","fardin"];
+
+// a.splice(1,0,"Ajoy");
+// document.write(a + "<br>");
+
+// if(Array.isArray(a)){
+//     document.write("This is a array")
+// }
+// else{
+//     document.write("This is not a array")
+// }
+
+// includs --------------------------------------------
+// var a=["harry","john","nick"];
+// var b=a.includes("harry");
+// document.write(b);
+
+// some ---------------------------------
+// always have to make a funsion to use some ==================
+
+// var a=[55,40,23,12];
+// document.write(a);
+// var b=a.some(checkAdult);
+// document.write(b);
+// function checkAdult(age){
+//     return age<18;
+// }
+ 
+
+// every ----------------------------------------
+// var a=[3,4,2,4];
+// document.write(a);
+// var b=a.every(adult);
+// document.write(b);
+// function adult(adult1){
+//     return(adult1<=18);
+// }
+// document.write("<br>")
+
+
+
+
+
+// find-------------------------------
+// var a=[34,33,4,32,21];
+// document.write(a+"<br>");
+
+// var b=a.find(adultage);
+// document.write(b+"<br>");
+// function adultage(age){
+//     return (age<18);
+// }
+
+
+// findIndex ================================
+
+// var a=[84,3212,4,23];
+// document.write(a+"<br>");
+// b=a.findIndex(adults);
+// document.write(b)
+// function adults(age){
+//     return (age<18);
+// }
+
+
+
+
+// // filter 
+// var a=[2,53,23,4,55];
+// document.write(a+"<br>");
+
+// b=a.filter(adultages);
+// document.write(b);
+// function adultages(age){
+//     return (age<18);
+// }
+
+
+
+// for each-------------------------
+// var a=[33,53,12,34];
+
+// a.forEach(function(adult,index){
+//     document.write(index+":"+adult*2+"<br>")
+// })
+
+
+
+// var x=[23,23,23,23];
+// x.forEach(function(ax,bx){
+//     document.write(bx+":"+ax*10+"<br>")
+// })
+// or--
+// var x=[23,12,13,55];
+// x.forEach(loop);
+// function loop(index,value){
+//     document.write(value+":"+index+"<br>")
+// }
+
+// var a=[12,23,12,123,132];
+// a.forEach(forr);
+// function forr (index,age) {
+//     document.write(age+":"+index+"<br>")
+// }
+
+// objects--------------------------------------------------------
+// var a={
+//     name:'Arfan',
+//     age:19,
+//     adress:'Narayanganj',
+//     numbers:[33,5434,234,654],
+//     fname:{
+//         'f':'a',
+//         'na':'r',
+//         'me':'fan'
+//     },
+//     movies:function howMany(){
+//         return 33;
+//     }
+// }
+// document.write(a.movies());
+// document.write(a.fname.f)
+// document.write(a.fname.na)
+// document.write(a.fname.me)
+
+// another way to write arrays------------------------
+// var a=new Object();
+// a.firstName="arfan";
+// a.lastName="ahmed";
+// a.age=19;
+// document.write(a.firstName)
+
+// array of objects----------------------------------------------
+// var im=[
+//     {name:'arfan',age:19},
+//     {name:'safin',age:19},
+// ]
+// for(a=0;a<im.length;a++){
+//     document.write(im[a].name+im[a].age+"<br>")
+// }
+
+// change values inside const ------------------------------------------
+// const names=["arfan","safin","mommy","ahmed"];
+
+// names[1]="arfan"
+// document.write(names);
+
+// const obj={
+//     name:"Arfan",
+//     age:19
+// }
+// obj.name="safin";
+// document.write(obj.name+obj.age);
+
+// for in loop-----------------------------------------------------------
+
+// var a={
+//     name:"Arfan",
+//     age:18
+// }
+// for (var key in a){
+//     document.write(a[key])
+// }
+
+// var c={
+//     name:"a",
+//     age:18
+// }
+// for(var d in c){
+//     document.write(c[d])
+// }
+// var a={
+//     name:"a",
+//     age:19
+// }
+// for(var )
+// // map----------------------------------------------------------------------------------
+// var a=[12,32,34,23];
+
+// var b=a.map(demo);
+// document.write(b)
+
+// function demo(x) {
+//     return x*34
+// }
+
+// var d=[
+//     {name:"arfan",age:19},
+//     {name:"arfan",age:19}
+// ];
+// var e=d.map(he);
+// document.write(e)
+// function he(k) {
+//     return k.name+" "+k.age
+// }
+
+// number methods-------------------------------------
+
+// var a=23.34533;
+// var b=a.toFixed(3)
+// document.write(b)
+
+// math methods
+// ceil 
+// var a=Math.ceil(34.333);
+// document.write(a)
+
+// floor
+// var a=Math.floor(34.57)
+// document.write(a)
+// round
+
+// var a=Math.round(3.34);
+// document.write(a)
+
+// trunc 
+// var a=Math.trunc(34.22);
+// document.write(a)
+
+// max 
+// var a=Math.max(23,22,43,45);
+// document.write(a)
+
+// min 
+// var a=Math.min(34,22,24,21);
+// document.write(a);
+
+// sqrt 
+// var a=Math.sqrt(4);
+// document.write(a)
+
+// crbt 
+// var a=Math.cbrt(125);
+// document.write(a);
+
+// power
+// var a=Math.pow(5,3);
+// document.write(a)
+
+// random 
+// var a=Math.floor(Math.random()*10)+1;
+// document.write(a)
+
+// abs
+// var a=Math.abs(23.50);
+// document.write(a)
+
+// pi 
+// var a=Math.PI;
+// document.write(a);
+
+// date object---------------------------------------
+// var a=new Date();
+// document.write(a)
+
+
+// get date 
+// var a=new Date();
+// toDateString
+// document.write(a.toDateString(22))
+
+// getDate 
+// document.write(a.getDate())
+
+// getFullYear
+// document.write(a.getFullYear())
+
+// getMonths
+// document.write(a.getMonth())
+
+// getDay
+// document.write(a.getDay())
+
+// getHours
+// document.write(a.getHours())
+
+// getMinites
+// document.write(a.getMinutes())
+
+// getSecounds
+// document.write(a.getSeconds())
+
+// getMiliseconds
+// document.write(a.getMilliseconds())
+
+// setDate 
+// a.setDate(3);
+// document.write(a);
+
+// setFullYear
+// setHours
+// setMiliseconds
+// setMinutes
+// setMonth
+// setSeconds
+
+
+// dom ---------------------==============================================
+// var element;
+// element=document.domain;
+// console.log(element);
+
+
+// get ---------------------------------------------
+// var element;
+// element=document.getElementById("js").getAttribute("class").valueOf
+// ;
+// console.log(element)
+
+// set --------------------------------------------------
+// document.getElementById("yo").innerHTML="hello";
+// document.getElementById("yo").setAttribute("class","he");
+// document.getElementById("yo").attributes[0].value="he";
+// document.getElementById("yo").removeAttribute("class")
+// element=document.getElementById("yo").getAttribute("class");
+// console.log(element)
+
+// quarry selector------------------------------
+
+
+
+// var a=document.querySelectorAll("a")
+
+// console.log(a)
+
+
+// dom addevent ----------------------------------
+// document.getElementById("link").addEventListener("mouseover",function () {
+//     this.style.filter="blur(2px)"
+// })
+
+// document.getElementById("link").addEventListener("mouseout",function () {
+//     this.style.backgroundColor="red",
+//    this.style.filter="blur(0px)"
+
+// })
+
+
+// dom class list methods
+// document.getElementById("link").classList.add("xyx","efs")
+// document.getElementById("link").classList.remove("xyx","efs")
+// toggle----------------------
+// document.getElementById("accordion").addEventListener("click",function () {
+//  document.getElementById("open").classList.toggle("open-now")   
+// })
+
+
+// contains
+
+// var a=document.getElementById("accordion").classList.contains("accordion");
+// console.log(a)
+
+// item --------------
+// var a=document.getElementById("accordion").classList.item(0);
+// console.log(a)
+
+// length-----------
+// var a=document.getElementById("accordion").classList.length
+// console.log(a);
+
+// dom traversal methods ----------------------------
+// var a=document.getElementById("divs").children[1]
+// console.log(a);
+// dom append methods 
+// var a=document.createElement('li');
+// var b=document.createTextNode('33');
+// a.appendChild(b);
+// var l=document.getElementById('divs');
+// var f=l.children[2];
+// l.replaceChild(a,f);
+
+// var copy=document.getElementById("divs").children[3];
+// var target=copy.cloneNode(true);
+// document.getElementById("div2").appendChild(target)
+
+
+
+// var target=document.getElementById("divs").children[3];
+// var copy=target.cloneNode(true);
+
+// document.getElementById("div2").appendChild(copy);
+
